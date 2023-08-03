458a97ec10560b22ffe30561e491d863ebb5a12c
Xasa7980 <99562106+Xasa7980@users.noreply.github.com>
2023-08-03 11:22:13 +0200

    added some scripts
    scripts : animation container, action signals, interface of interactuables, interactuables father scriptable object.

---
+++ DungeonSurvival/Assets/03_Scripts/00_Player/ActionSignals.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class ActionSignals
+{
+    public static UnityAction onGameOver;
+    public static UnityAction<MonoBehaviour> onTargetAction;
+}
