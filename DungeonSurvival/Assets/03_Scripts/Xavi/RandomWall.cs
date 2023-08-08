8ec1dfd3dfbb8182b853efcc674282175b8e58d7
Xasa7980 <99562106+Xasa7980@users.noreply.github.com>
2023-08-08 19:25:04 +0200

    added rooms and scriptable objects for items

---
+++ DungeonSurvival/Assets/03_Scripts/Xavi/RandomWall.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RandomWall : MonoBehaviour
+{
+    public float amountToCreate;
+    public GameObject[] walls;
+    public float[] rating;
+    private void OnEnable()
+    {
+        GetWall();
+    }
+    void GetWall()
+    {
+        for (int i = 0; i < amountToCreate; i++)
+        {
+            List<GameObject> gOs = new();
+            gOs.Add(Instantiate(walls[(int)RarityRating()], transform));
+            foreach (var item in gOs)
+            {
+                item.transform.position = transform.GetChild(0).position + transform.right * i * 20;
+                item.SetActive(true);
+            }
+        }
+    }
+    float RarityRating()
+    {
+        float total = 0;
+        foreach (var item in rating)
+        {
+            total += item;
+        }
+        float randNum = Random.value * total;
+        for (int i = 0; i < rating.Length; i++)
+        {
+            if(randNum < rating[i])
+            {
+                return i;
+            }
+            else
+            {
+                randNum -= rating[i];
+            }
+        }
+        return rating.Length - 1;
+
+    }
+}
