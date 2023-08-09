7f85e5df375c1f727a372d4d8d4a1db812fbcf3f
DavidLambert1993 <davidlambertindiedev@gmail.com>
2023-08-09 17:10:04 -0400

    Reorganized Assets && Added Chest Interactable base

--- DungeonSurvival/Assets/03_Scripts/Tools/RandomWall.cs
+++ DungeonSurvival/Assets/03_Scripts/Tools/RandomWall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class randomwall : MonoBehaviour
+public class RandomWall : MonoBehaviour
 {
     public float amountToCreate;
     public GameObject[] walls;
